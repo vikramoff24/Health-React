be96fcbc50a6edfa8fd3f6cbf442f1080eaaaba3
vikramoff24 <vikram.senthil@outlook.com>
2021-10-16 17:42:13 +0530

    Folder structured for development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
+import "./static/style/common/shared.css";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from "react-router-dom";
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>hello world</h1>
+      <Fragment>
+        <Switch></Switch>
+      </Fragment>
+    </Router>
   );
 }
-
 export default App;
